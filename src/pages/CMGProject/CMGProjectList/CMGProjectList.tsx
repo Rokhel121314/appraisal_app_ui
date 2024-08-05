38e86c0805ce610ef3837211269b79790de23b29
Rokhel121314 <jerickddelacruz@gmail.com>
2024-08-05 13:21:50 +0800

    temporary home page

--- src/pages/CMGProject/CMGProjectList/CMGProjectList.tsx
+++ src/pages/CMGProject/CMGProjectList/CMGProjectList.tsx
@@ -1,7 +1,7 @@
 import styles from "./styles.module.css";
 
 const CMGProjectList = () => {
-  return <div className={styles.container}>CMGProjectList</div>;
+  return <div className={styles.container}>UNDER DEVELOPMENT</div>;
 };
 
 export default CMGProjectList;
--- src/pages/SPALLCHome/SPALLCHome.tsx
+++ src/pages/SPALLCHome/SPALLCHome.tsx
@@ -1,21 +1,33 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./style.module.css";
+import Button from "../../components/Button/Button";
 
 const SPALLCHome = () => {
   const navigate = useNavigate();
   return (
     <main className={styles.container}>
-      <div
-        className={styles.section_gallagher}
-        onClick={() => navigate("/spallc/gallagher")}
-      >
-        GALLAGHER PROJECTS
+      <div className={styles.section}>
+        <div className={styles.button_container}>
+          <Button
+            title="VIEW Gallagher Projects"
+            disabled={false}
+            onClick={() => {
+              navigate("/spallc/gallagher");
+            }}
+          />
+        </div>
       </div>
-      <div
-        className={styles.cmg}
-        onClick={() => navigate("/spallc/catholic-mutual")}
-      >
-        CATHOLIC MUTUALS PROJECTS
+
+      <div className={styles.section}>
+        <div className={styles.button_container}>
+          <Button
+            title="VIEW CMG Projects"
+            disabled={false}
+            onClick={() => {
+              navigate("/spallc/catholic-mutual");
+            }}
+          />
+        </div>
       </div>
     </main>
   );
